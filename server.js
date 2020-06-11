3669fd05d42c8d1f8919901b00dd85a06d0f475d
unknown <jenearly@gmail.com>
2020-06-11 18:29:24 -0400

    express server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
