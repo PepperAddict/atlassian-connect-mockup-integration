303f6537cfe8cc8f6107a3af06d34d8c388c7daa
unknown <jenearly@gmail.com>
2020-06-16 16:12:07 -0400

    deployment step for production

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 8080
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(port);
+
+console.log('started on port ' + port)
